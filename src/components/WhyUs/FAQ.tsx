739c9f092c627228f81d0a5896672406fa366a67
Andreyhuey <andreyhuey777@gmail.com>
2023-08-24 00:32:19 +0100

    Frequently asked questions component completed

--- src/components/WhyUs/FAQ.tsx
+++ src/components/WhyUs/FAQ.tsx
@@ -1,10 +1,100 @@
+"use client";
+
 import React from "react";
+import { AiOutlineDown } from "react-icons/ai";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
 
 const FAQ = () => {
   return (
-    <div className="min-h-screen container py-16 text-[#272D40]">
+    <div className="min-h-screen container pt-16 text-[#272D40]">
       <p className="h2 text-center">Frequently asked questions</p>
-      <div className=""></div>
+      <div className="w-full text-[#272D40] pt-10">
+        <Accordion allowZeroExpanded={true} className="flex flex-col gap-4">
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                What can Slack apps do?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+          {/*  */}
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                How do I install apps in Slack?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+          {/*  */}
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                Are Slack apps free?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+          {/*  */}
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                How do I build a Slack app?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+          {/*  */}
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                How do I create a workflow in Slack?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+          {/*  */}
+          <AccordionItem className="border-b-[1px] border-[#99999E]">
+            <AccordionItemHeading>
+              <AccordionItemButton className="flex items-center justify-between gap-1 w-full bg-[#fff] py-4 px-2">
+                How do I become a Slack technology partner?
+                <AiOutlineDown />
+              </AccordionItemButton>
+            </AccordionItemHeading>
+            <AccordionItemPanel className="p-2">
+              Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+              ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum
+            </AccordionItemPanel>
+          </AccordionItem>
+        </Accordion>
+      </div>
     </div>
   );
 };
