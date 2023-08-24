b9d83186a5081656433d3a449a8a266195b0603b
Andreyhuey <andreyhuey777@gmail.com>
2023-08-24 10:41:51 +0100

    Pricing page completed

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,6 +2,8 @@ import { Footer, Navbar, Newsletter } from "@/components";
 import Image from "next/image";
 import React from "react";
 import pricelist from "../../assets/pricelist.svg";
+import logo from "../../assets/Group-1145.svg";
+import { BiLogoTwitter } from "react-icons/bi";
 
 const page = () => {
   return (
@@ -65,7 +67,39 @@ const page = () => {
             </div>
           </div>
           <Image src={pricelist} alt="price list" className="py-14" />
-          <div className="py-8"></div>
+          <div className="py-8 flex flex-col items-center justify-center gap-5 text-center">
+            <p className="h1">Ready for take-off?</p>
+            <p className="md:max-w-[450px]">
+              Download the Slack app and start taking your productivity to new
+              heights
+            </p>
+            <button className="py-3 px-14 hover:text-[#3D42B0] text-white hover:bg-white bg-[#1F2492] rounded border-[#99999E] border w-full md:w-auto">
+              Download for Mac
+            </button>
+            <p className="text-secondary">or</p>
+            <button className="py-3 md:px-14 px-10 text-secondary rounded border-[#99999E] border w-full md:w-auto">
+              $ brew install - - cask slack
+            </button>
+            <p className="text-secondary">macOS 11+</p>
+          </div>
+          <div className="py-8 flex flex-col md:flex-row items-center justify-center gap-x-4 gap-y-6 text-black">
+            <div className="flex flex-col items-start justify-start gap-4 shadow-2xl px-5 py-9 rounded-md">
+              <BiLogoTwitter className="text-[30px]" />
+              <p className="font-bold">Stay up to date</p>
+              <p className="md:max-w-[400px]">
+                We’d love to stay connected with you. If the feeling’s mutual,
+                follow @slackapp on Twitter for the latest news and updates.
+              </p>
+            </div>
+            <div className="flex flex-col items-start justify-start gap-4 shadow-2xl px-5 py-9 rounded-md">
+              <Image src={logo} alt="logo" className="h-[30px]" />
+              <p className="font-bold">Help shape the product</p>
+              <p className="md:max-w-[400px]">
+                Lots of the best brightness ideas come from you. Join our Slack
+                Community to help create the future of Slack.
+              </p>
+            </div>
+          </div>
         </div>
       </>
       <Newsletter />
