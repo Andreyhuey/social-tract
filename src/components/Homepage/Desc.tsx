87c0ddfaf5101f53d56780e3385d97a9383011f4
Andreyhuey <andreyhuey777@gmail.com>
2024-08-02 22:26:22 +0100

    fix: add structure to child components

--- src/components/Homepage/Desc.tsx
+++ src/components/Homepage/Desc.tsx
@@ -11,8 +11,8 @@ const Desc = () => {
   return (
     <div>
       {/* */}
-      <div className="bg-[#F9F9F9] min-h-screen py-8">
-        <div className="container py-12">
+      <div className="bg-[#F9F9F9] py-8">
+        <div className="container py-10">
           <div className="flex flex-col lg:flex-row items-center justify-between gap-5">
             <div className="text-primary">
               <div>
@@ -52,87 +52,86 @@ const Desc = () => {
         </div>
       </div>
       {/*  */}
-      <div className="min-h-screen py-8">
-        <div className="container py-12">
-          <div className="flex flex-col lg:flex-row items-center justify-between gap-5">
-            <Image src={image4} alt="image 4" className="md:order-1 order-2" />
-            <div className="text-primary md:order-2 order-1">
-              <div>
-                <p className="">Every decision, made searchable</p>
-                <p className="md:max-w-[360px] md:text-[32px] text-[25px] text-black font-bold">
-                  Where all knowledge is common knowledge
-                </p>
-                <p className="md:max-w-[430px]">
-                  You don’t have to go to every meeting and be in every
-                  conversation to be in the loop. The shared knowledge of your
-                  whole company is out there, organized, and accessible when
-                  (and if) you want it.
-                </p>
-                <button className="underline text-[#50519E]">Learn More</button>
-              </div>
-              <div className="mt-9 pt-8 border border-t-[#99999E] border-r-0 border-b-0 border-l-0 md:max-w-[380px]">
-                <div className="flex items-start justify-between gap-3">
-                  <Image src={clifford} alt="image of mark" />
-                  <div>
-                    <p className="italic">
-                      “The fantastic thing about Slack is transparency. Everyone
-                      can see what everyone else is working on and the status of
-                      thing is much more visible.”
-                    </p>
-                    <p className="pt-3 font-bold">
-                      Clifford Bailey, Ocado Technology{" "}
-                      <span className="text-[#50519E] font-normal">
-                        {" "}
-                        - Read story
-                      </span>
-                    </p>
-                  </div>
+
+      <div className="container py-10">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-5">
+          <Image src={image4} alt="image 4" className="md:order-1 order-2" />
+          <div className="text-primary md:order-2 order-1">
+            <div>
+              <p className="">Every decision, made searchable</p>
+              <p className="md:max-w-[360px] md:text-[32px] text-[25px] text-black font-bold">
+                Where all knowledge is common knowledge
+              </p>
+              <p className="md:max-w-[430px]">
+                You don’t have to go to every meeting and be in every
+                conversation to be in the loop. The shared knowledge of your
+                whole company is out there, organized, and accessible when (and
+                if) you want it.
+              </p>
+              <button className="underline text-[#50519E]">Learn More</button>
+            </div>
+            <div className="mt-9 pt-8 border border-t-[#99999E] border-r-0 border-b-0 border-l-0 md:max-w-[380px]">
+              <div className="flex items-start justify-between gap-3">
+                <Image src={clifford} alt="image of mark" />
+                <div>
+                  <p className="italic">
+                    “The fantastic thing about Slack is transparency. Everyone
+                    can see what everyone else is working on and the status of
+                    thing is much more visible.”
+                  </p>
+                  <p className="pt-3 font-bold">
+                    Clifford Bailey, Ocado Technology{" "}
+                    <span className="text-[#50519E] font-normal">
+                      {" "}
+                      - Read story
+                    </span>
+                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       {/*  */}
-      <div className="min-h-screen py-8">
-        <div className="container py-12">
-          <div className="flex flex-col lg:flex-row items-center justify-between gap-5">
-            <div className="text-primary">
-              <div>
-                <p className="">The hub all work</p>
-                <p className="md:max-w-[360px] md:text-[32px] text-[25px] text-black font-bold">
-                  We create world-class digital products materials that
-                  communicate clearly.
-                </p>
-                <p className="md:max-w-[430px]">
-                  If all the people are in Slack, and all your apps, resources
-                  and tools are part of the conversation, you don’t have to put
-                  in extra work to connect them. They’re just there, and youre
-                  all on the same page, ready to go.
-                </p>
-                <button className="underline text-[#50519E]">Learn More</button>
-              </div>
-              <div className="mt-9 pt-8 border border-t-[#99999E] border-r-0 border-b-0 border-l-0 md:max-w-[380px]">
-                <div className="flex items-start justify-between gap-3">
-                  <Image src={nick} alt="image of mark" />
-                  <div>
-                    <p className="italic">
-                      “Slack is not only great at connecting people, it’s great
-                      at connect- ing the tools and systems we already have.”
-                    </p>
-                    <p className="pt-3 font-bold">
-                      Nick Coronges, R/GA{" "}
-                      <span className="text-[#50519E] font-normal">
-                        {" "}
-                        - Read story
-                      </span>
-                    </p>
-                  </div>
+
+      <div className="container py-10">
+        <div className="flex flex-col lg:flex-row items-center justify-between gap-5">
+          <div className="text-primary">
+            <div>
+              <p className="">The hub all work</p>
+              <p className="md:max-w-[360px] md:text-[32px] text-[25px] text-black font-bold">
+                We create world-class digital products materials that
+                communicate clearly.
+              </p>
+              <p className="md:max-w-[430px]">
+                If all the people are in Slack, and all your apps, resources and
+                tools are part of the conversation, you don’t have to put in
+                extra work to connect them. They’re just there, and youre all on
+                the same page, ready to go.
+              </p>
+              <button className="underline text-[#50519E]">Learn More</button>
+            </div>
+            <div className="mt-9 pt-8 border border-t-[#99999E] border-r-0 border-b-0 border-l-0 md:max-w-[380px]">
+              <div className="flex items-start justify-between gap-3">
+                <Image src={nick} alt="image of mark" />
+                <div>
+                  <p className="italic">
+                    “Slack is not only great at connecting people, it’s great at
+                    connect- ing the tools and systems we already have.”
+                  </p>
+                  <p className="pt-3 font-bold">
+                    Nick Coronges, R/GA{" "}
+                    <span className="text-[#50519E] font-normal">
+                      {" "}
+                      - Read story
+                    </span>
+                  </p>
                 </div>
               </div>
             </div>
-            <Image src={image5} alt="image 5" />
           </div>
+          <Image src={image5} alt="image 5" />
         </div>
       </div>
     </div>
