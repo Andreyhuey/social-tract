2103f12a32efc835319876593b18d2e36134ba24
Andreyhuey <andreyhuey777@gmail.com>
2024-08-02 22:27:35 +0100

    update: add structure to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,10 +13,10 @@ const Navbar = () => {
 
   return (
     <div className="fixed left-0 top-0 z-30 w-full h-auto bg-white">
-      <div className="container text-black">
+      <div className=" text-black container">
         {/* for tabs and pcs */}
         <div className="hidden md:flex md:justify-between md:items-center py-4 relative">
-          <ul className="flex flex-1 items-center justify-start gap-7">
+          <ul className="flex items-center justify-start gap-7">
             <li className="cursor-pointer">
               <Link href="why-slack">Why Slack?</Link>
             </li>
@@ -30,7 +30,7 @@ const Navbar = () => {
 
           <Link
             href={"/"}
-            className="flex flex-1 items-center justify-center gap-2 
+            className="flex items-center justify-center gap-2 
           absolute top-1/2 left-1/2 
           transform -translate-x-1/2 -translate-y-1/2 cursor-pointer
           "
@@ -38,7 +38,7 @@ const Navbar = () => {
             <Image src={logo} alt="site logo" />
             <p className="text-[18px] font-semibold">slack</p>
           </Link>
-          <div className="flex flex-1 items-center justify-end gap-5">
+          <div className="flex items-center justify-end gap-5">
             <button>Sign in</button>
             <button className="border border-[#99999E] py-2 px-3 hover:text-white hover:bg-[#1F2492] rounded ">
               Sign up
